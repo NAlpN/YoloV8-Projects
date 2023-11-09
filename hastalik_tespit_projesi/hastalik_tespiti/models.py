6e370badb04e9eaa60195f71b02b568a6c05f003
AlpN <alpnn52@gmail.com>
2023-11-09 08:54:59 +0300

    Kemik kodlar oluşturuldu

--- hastalik_tespit_projesi/hastalik_tespiti/models.py
+++ hastalik_tespit_projesi/hastalik_tespiti/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Hastalik(models.Model):
+    ad = models.CharField(max_length=100)
+    # Diğer alanlar buraya eklenecek
+
+    def __str__(self):
+        return self.ad
--- hastalik_tespit_projesi/hastalik_tespiti/views.py
+++ hastalik_tespit_projesi/hastalik_tespiti/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Hastalik
 
-# Create your views here.
+def hastalik_tespit_view(request):
+
+    # Model ile ilgili tespit işlemleri burada yapılacak
+    # Sonuçlar context'e eklenecek ve template'e gönderilecek
+   
+    hastaliklar = Hastalik.objects.all()
+    context = {'hastaliklar': hastaliklar}
+    return render(request, 'hastalik_tespiti/hastalik_tespit.html', context)
