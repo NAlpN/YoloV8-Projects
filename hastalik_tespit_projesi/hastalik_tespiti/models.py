22f4f725881ee6f8b3d860af44f5d182f50bdab8
AlpN <alpnn52@gmail.com>
2023-11-09 09:00:02 +0300

    Arayüz denemesi işlemleri kaldırıldı

--- hastalik_tespit_projesi/hastalik_tespiti/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Hastalik(models.Model):
-    ad = models.CharField(max_length=100)
-    # Diğer alanlar buraya eklenecek
-
-    def __str__(self):
-        return self.ad
